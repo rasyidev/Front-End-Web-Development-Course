cac6d9a6c1e4e49f07821ffd385ccab3c196a691
rasyidev <habib.rasyid11@gmail.com>
2020-12-30 10:19:36 +0700

    Change root project directory

---
+++ Material/1-constructor.js
@@ -0,0 +1,10 @@
+class Car {
+   constructor(manufacture, color){
+      this.manufacture = manufacture;
+      this.color = color;
+      this.isEngineActive = false
+   }
+}
+
+let mycar = new Car("Avanza", "Silver");
+console.log(mycar)
