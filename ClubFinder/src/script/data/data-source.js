c2a3c4ba1adcc2fae59727b74048a2a2af520d9e
rasyidev <habib.rasyid11@gmail.com>
2021-01-03 22:44:25 +0700

    Modify DataSource class, implement the Promise materials

--- ClubFinder/src/script/data/data-source.js
+++ ClubFinder/src/script/data/data-source.js
@@ -1,17 +1,16 @@
 class DataSource {
-    constructor(onSuccess, onFailed){
-        this.onSuccess = onSuccess;
-        this.onFailed = onFailed;
-    }
-    searchClub(keyword) {
+    static searchClub(keyword){
         const filteredClubs = clubs.filter( club => {
-            return club.name.toUpperCase().includes(keyword.toUpperCase());
-        });
+                    return club.name.toUpperCase().includes(keyword.toUpperCase())
+                });
+        return new Promise((resolve, reject) => {
+            
+            if (filteredClubs.length) {
+                resolve(filteredClubs);
+            } else {
+                reject(`${keyword} +  is not found`);
+            }
+        })
     
-        if (filteredClubs.length) {
-            this.onSuccess(filteredClubs);
-        } else {
-            this.onFailed(`${keyword} +  is not found`);
-        }
-    };
+    }
 }
