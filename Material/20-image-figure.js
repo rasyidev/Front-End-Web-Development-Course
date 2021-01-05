b4ecdbe8f404d5cac9e2f21fad15c5b2a5d4a3c9
rasyidev <habib.rasyid11@gmail.com>
2021-01-06 06:11:19 +0700

    Add style to custom element without shadow dom

--- Material/20-image-figure.js
+++ Material/20-image-figure.js
@@ -14,6 +14,31 @@ class ImageFigure extends HTMLElement{
 
    render() {
       this.innerHTML = `
+
+         <style>
+        figure {
+          border: thin #c0c0c0 solid;
+          display: flex;
+          flex-flow: column;
+          padding: 5px;
+          max-width: 220px;
+          margin: auto;
+        }
+ 
+        figure > img {
+          max-width: 220px;
+        }
+ 
+        figure > figcaption {
+          background-color: #222;
+          color: #fff;
+          font: italic smaller sans-serif;
+          padding: 3px;
+          text-align: center;
+        }
+        
+      </style>
+
          <figure>
             <img src="${this.src}" alt="${this.alt}">
             <figcaption>${this.caption}</figcaption>
--- Material/20-main.js
+++ Material/20-main.js
@@ -8,13 +8,8 @@ if (!imageFigureElement){
 }
 
 // change or add value to the attribute caption.
-setTimeout(() => {
-   imageFigureElement.setAttribute("caption", "Image 1");
-}, 2000);
 
-setTimeout(() => {
-   imageFigureElement.setAttribute("src", "https://i.imgur.com/iJq78XH.jpg");
-   imageFigureElement.setAttribute("alt", "Dicoding Logo");
-   imageFigureElement.setAttribute("caption", "Huruf g dalam logo Dicoding");
-   document.body.appendChild(imageFigureElement);
-}, 5000)
+imageFigureElement.setAttribute("src", "https://i.imgur.com/iJq78XH.jpg");
+imageFigureElement.setAttribute("alt", "Dicoding Logo");
+imageFigureElement.setAttribute("caption", "Huruf g dalam logo Dicoding");
+document.body.appendChild(imageFigureElement);
